b16c74c8d7925f12e7df9af7776123bfab05b326
Daffy Liu <llrtime@bupt.edu.cn>
2017-12-27 22:39:57 +0800

    Update softmax.py

--- softmax.py
+++ softmax.py
@@ -6,12 +6,20 @@ def softmax(x):
     #axis=0是列，axis=1是行
     #axis=0是指第一个维度，axis=1是指第二个维度 所谓的第几个维度是x.shape()的结果
     #softmax满足所有元素相加=1
+    #使用性质，softmax不会随着输入向量偏移而偏移
     y = np.exp(x - np.max(x, axis=len(x.shape) - 1, keepdims=True))
     #print(np.max(x, axis=0, keepdims=True))
     #print(y)
     normalization = np.sum(y, axis=len(x.shape) - 1, keepdims=True)
     #print(normalization)
     return np.divide(y, normalization)
+
+def softmax__(x):
+    assert len(x.shape)>=2
+    x-=np.max(x,axis=1,keepdims=True)
+    print(x)
+    x=np.exp(x)/np.sum(np.exp(x),axis=1,keepdims=True)
+    return x
 
 def test_softmax_basic():
     print("Running basic tests...")
@@ -34,7 +42,9 @@ def test_softmax_basic():
 
 def test_softmax():
     print("Running your tests...")
+    test=softmax__(np.array([[1001,1002,1003],[18,5,100]]))
+    print(test)
 
 if __name__ == "__main__":
-    test_softmax_basic()
+    #test_softmax_basic()
     test_softmax()
